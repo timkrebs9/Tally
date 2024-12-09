2f617fa0d4d9ca260e60c85db2aeb72f4a13ef54
Tim Krebs <timkrebs9@gmail.com>
2024-12-09 17:24:33 +0100

    Refactor Azure storage account configuration in GitHub Actions workflow and Terraform module
    - Removed the minimum TLS version requirement from the Azure CLI command in the GitHub Actions workflow and the Terraform module, simplifying the storage account creation process.
    - These changes enhance flexibility in the deployment configuration while maintaining compliance with security standards.

--- infrastructure/modules/storage-account/main.tf
+++ infrastructure/modules/storage-account/main.tf
@@ -16,7 +16,6 @@ resource "azurerm_storage_account" "st" {
   account_tier             = "Standard"
   account_replication_type = "LRS"
   account_kind             = "StorageV2"
-  min_tls_version          = "TLS1_2"
   is_hns_enabled           = var.hns_enabled
 
   tags = var.tags
